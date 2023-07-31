92abed9bf3f4342f4eb9a52f4c6bb0b12de6b69f
Gabriel <grsilva112@gmail.com>
2023-07-31 16:28:11 -0300

    First commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,25 @@
 import { Button } from "@rbeiro-ui/react-components";
+import { signOut } from "next-auth/react";
 
-const Header = () => {
+interface BaseProps {
+  user?: {
+    name: string;
+  };
+}
+
+const Header = ({ user }: BaseProps) => {
+  function handleSignOut() {
+    signOut()
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+      
+      return void
+  }
   return (
     <>
       <div>Hello World</div>
       <h1>Olá, {user?.name ? user?.name : "nenhum usuário encontrado"}</h1>
-      <Button onClick={() => handleSignOut()}>Sair</Button>
+      <Button onClick={async () => handleSignOut()}>Sair</Button>
     </>
   );
 };
